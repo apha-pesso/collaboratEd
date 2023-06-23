b272bcb5219e04ed5811c80bcd3c4271b7ff827b
Mutiu Falade <35642827+apha-pesso@users.noreply.github.com>
2023-06-23 08:39:15 +0100

    Update redis.js

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -1,57 +1,57 @@
-// RedisClient class
-const redis = require('redis');
+// // RedisClient class
+// const redis = require('redis');
 
-const { createClient } = redis;
+// const { createClient } = redis;
 
-// Create a redis class
-class RedisClient {
-  constructor() {
-    this.client = createClient();
-    this.connected = true;
-    this.client.on('error', (err) => {
-      this.connected = false;
-      console.log(err);
-    });
-  }
+// // Create a redis class
+// class RedisClient {
+//   constructor() {
+//     this.client = createClient();
+//     this.connected = true;
+//     this.client.on('error', (err) => {
+//       this.connected = false;
+//       console.log(err);
+//     });
+//   }
 
-  // isAlive method to confirm the connection status
-  isAlive() {
-    return this.connected;
-  }
+//   // isAlive method to confirm the connection status
+//   isAlive() {
+//     return this.connected;
+//   }
 
-  // Asynchronous get method
-  async get(key) {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (err, value) => {
-        if (err) reject(err);
-        resolve(value);
-      });
-    });
-  }
+//   // Asynchronous get method
+//   async get(key) {
+//     return new Promise((resolve, reject) => {
+//       this.client.get(key, (err, value) => {
+//         if (err) reject(err);
+//         resolve(value);
+//       });
+//     });
+//   }
 
-  // Asynchronous set method
-  async set(key, value, duration) {
-    return new Promise((resolve, reject) => {
-      this.client.setex(key, duration, value, (err, reply) => {
-        if (err) reject(err);
-        resolve(reply);
-      });
-    });
-  }
+//   // Asynchronous set method
+//   async set(key, value, duration) {
+//     return new Promise((resolve, reject) => {
+//       this.client.setex(key, duration, value, (err, reply) => {
+//         if (err) reject(err);
+//         resolve(reply);
+//       });
+//     });
+//   }
 
-  // Asynchronous del method
-  async del(key) {
-    return new Promise((resolve, reject) => {
-      this.client.del(key, (err, reply) => {
-        if (err) reject(err);
-        resolve(reply);
-      });
-    });
-  }
-}
+//   // Asynchronous del method
+//   async del(key) {
+//     return new Promise((resolve, reject) => {
+//       this.client.del(key, (err, reply) => {
+//         if (err) reject(err);
+//         resolve(reply);
+//       });
+//     });
+//   }
+// }
 
-// Create a new instance of the RedisClient class
-const redisClient = new RedisClient();
+// // Create a new instance of the RedisClient class
+// const redisClient = new RedisClient();
 
-// Export the redisClient instance
-module.exports = redisClient;
+// // Export the redisClient instance
+// module.exports = redisClient;
